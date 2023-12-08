da2f807548f5d56fb4965e8c94aef19cd9324b09
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2023-12-08 17:47:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function threeSum(arr, target) {
 // write your code here
 	let currDiff=Number.MAX_VALUE;
+	let x=0;
 	for(let i=0;i<arr.length;i++){
 		for(let j=i+1;j<arr.length;j++){
 			for(let k=j+1;k<arr.length;k++){
 				let sum=arr[i]+arr[j]+arr[k];
-				let diff=target-sum;
+				
+				let diff=Math.abs(target-sum);
+				
 					if(diff<currDiff){
 						currDiff=diff
-							let x=sum;
+						   x=sum;
 					}
 			}
 		}
